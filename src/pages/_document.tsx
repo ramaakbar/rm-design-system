15c669289447a950e5eb61b0c42fc79ece1b6a89
rama <akbar.6b@gmail.com>
2023-04-22 21:49:43 +0700

    fix some stuff, adjust dark color

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,13 @@ import { Head, Html, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <script
+          defer
+          data-domain="ui.ramaakbar.xyz"
+          src="https://analytics.ramaakbar.xyz/js/script.js"
+        ></script>
+      </Head>
       <body className="bg-background font-sans text-foreground antialiased">
         <Main />
         <NextScript />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,21 +8,11 @@ export default function Home() {
         system, or even contribute to make it better!
       </p>
       <p className="max-w-xl">Heavily Inspired by:</p>
-      <div className="flex flex-col">
+      <div className="inline-flex flex-col">
         <a href="https://aether.thcl.dev">Aether Design System</a>
         <a href="https://ui.shadcn.com"> Shad ui</a>
         <a href="https://reshaped.so"> Reshaped</a>
       </div>
-      <label className="block">
-        <span className="text-gray-700">Full name</span>
-        <input
-          type="text"
-          name="first-name"
-          id="first-name"
-          placeholder="name"
-          className="block w-full rounded-md border-0 bg-transparent px-3 py-2 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600"
-        ></input>
-      </label>
     </>
   );
 }
